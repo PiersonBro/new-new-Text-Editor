66c32eb9188ce92aaebaa753652fc6134e957e36
= <EandZ@EZs-MacBook-Pro.local>
2012-12-29 22:04:01 -0800

    Deleted A bunch of files as well as added a name to the storyBoard.

--- new-new-Text-Editor/IDTWebViewController.m
+++ new-new-Text-Editor/IDTWebViewController.m
@@ -28,6 +28,7 @@
     
     
     [super viewDidLoad];
+    //This load the string set by the detail view controller.
     [self.webView loadHTMLString:self.stringForWebView baseURL:nil];
     
 	// Do any additional setup after loading the view.
